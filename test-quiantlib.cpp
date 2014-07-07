ec37ae97d1f7636af438a7b3759edf0c41249235
Saul Joseph Wiggin <saulwiggin@googlemail.com>
2014-07-07 10:54:49 +0100

    write code for time value of money using quantlib

---
+++ test-quiantlib.cpp
@@ -0,0 +1,15 @@
+//first line is required in .NET
+#include "stdafx.h"
+#include <iostream>
+#include <ql/quantlib.hpp>
+
+using namespace std;
+using namespace Quantlib.hpp;
+
+void main()
+{
+	boost::shared_ptr<money> m(new Money);
+	cout << m->value() << end1;
+
+	getchat();
+}
